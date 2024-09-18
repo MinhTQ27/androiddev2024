b17da71fee31d55e572776f37a173a5d27123230
MinhTQ27 <minhtq.ba12-127@st.usth.edu.vn>
2024-09-18 22:49:09 +0700

    Upload prac8

--- USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -9,48 +9,30 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+
+
 public class WeatherActivity extends AppCompatActivity {
 
-    private static final String TAG = "WeatherActivity";
-    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "onCreate is called");
         setContentView(R.layout.activity_weather);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ViewPager viewPager = findViewById(R.id.viewpager);
-        FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager());
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(WeatherAndForecastFragment.newInstance(), "Ha Noi,Viet Nam");
+        adapter.addFragment(WeatherAndForecastFragment.newInstance(), "Paris,France");
+        adapter.addFragment(WeatherAndForecastFragment.newInstance(), "Thuong Hai, China ");
         viewPager.setAdapter(adapter);
-    }
-
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart is called");
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume is called");
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(TAG, "onPause is called");
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG, "onStop is called");
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy is called");
     }
 }
